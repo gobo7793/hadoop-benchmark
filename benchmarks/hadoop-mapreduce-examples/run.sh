50d946dafd3786ae954d891fe76edf80bf89f65a
Gerald Siegert <gerald.siegert@outlook.com>
2018-01-22 17:15:12 +0100

    fixed for using with selfbalancing approach

--- benchmarks/hadoop-mapreduce-examples/run.sh
+++ benchmarks/hadoop-mapreduce-examples/run.sh
@@ -2,12 +2,15 @@
 set -e
 source $(dirname $0)/../common.sh
 
+#original container:
+#hadoop-benchmark/hadoop \
+
 docker $controller_conn run \
   -t \
   --rm \
   --net hadoop-net \
   --name hadoop-mapreduce-examples \
   -h hadoop-mapreduce-examples \
-  hadoop-benchmark/hadoop \
+  hadoop-benchmark/self-balancing-mt \
   run \
   hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.1.jar "$@"
