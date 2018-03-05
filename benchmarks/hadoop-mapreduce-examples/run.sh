d6b7cc26d1005023bc092154a8c609de5850fcd0
Gerald Siegert <gerald.siegert@outlook.com>
2018-03-05 15:17:55 +0100

    start exec in controller

--- benchmarks/hadoop-mapreduce-examples/run.sh
+++ benchmarks/hadoop-mapreduce-examples/run.sh
@@ -2,20 +2,21 @@
 set -e
 source $(dirname $0)/../common.sh
 
-if [[ $1 == "-t" ]]; then
-  name="examples-$timestamp"
-  shift
-else
-  name="hadoop-mapreduce-examples"
-fi
+#if [[ $1 == "-t" ]]; then
+#  name="examples-$timestamp"
+#  shift
+#else
+#  name="hadoop-mapreduce-examples"
+#fi
 
-cmd="docker $controller_conn run \
-  -t \
-  --rm \
-  --net hadoop-net \
-  --name $name \
-  -h $name \
-  hadoop-benchmark/self-balancing-mt \
-  run \
-  hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.1.jar $@"
+#cmd="docker $controller_conn run \
+#  -t \
+#  --rm \
+#  --net hadoop-net \
+#  --name $name \
+#  -h $name \
+#  hadoop-benchmark/self-balancing-mt \
+#  run \
+#  hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.1.jar $@"
+cmd="docker $controller_conn exec controller hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.1.jar $@"
 $cmd
