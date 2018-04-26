1203c9aa61906ca043aff447539fab45d1ba8d3e
Gerald Siegert <gerald.siegert@outlook.com>
2018-04-26 10:20:38 +0200

    made benchmarks running on mh cluster

--- benchmarks/common.sh
+++ benchmarks/common.sh
@@ -3,8 +3,13 @@ set -e
 
 [[ -f $CONFIG ]] && source $CONFIG
 
-CLUSTER_NAME_PREFIX=${CLUSTER_NAME_PREFIX:-'local-hadoop'}
+CLUSTER_NAME_PREFIX=${CLUSTER_NAME_PREFIX:-'hadoop'}
 
-declare -r controller_node_name="$CLUSTER_NAME_PREFIX-controller"
-declare -r controller_conn="$(docker-machine config $controller_node_name)"
 declare -r timestamp=`date +%s%N | cut -b3-14`
+if [[ $(docker-machine status hadoop-controller) -eq "Running" ]]; then
+    declare -r controller_node_name="$CLUSTER_NAME_PREFIX-controller"
+    declare -r controller_conn="$(docker-machine config $controller_node_name)"
+else
+    declare -r controller_node_name=""
+    declare -r controller_conn=""
+fi
--- benchmarks/hadoop-jobclient-tests/run.sh
+++ benchmarks/hadoop-jobclient-tests/run.sh
@@ -2,21 +2,21 @@
 set -e
 source $(dirname $0)/../common.sh
 
-#if [[ $1 == "-t" ]]; then
-#  name="jobclient-$timestamp"
-#  shift
-#else
-#  name="hadoop-jobclient-tests"
-#fi
+if [[ $1 == "-t" ]]; then
+  name="jobclient-$timestamp"
+  shift
+else
+  name="hadoop-jobclient-tests"
+fi
 
-#cmd="docker $controller_conn run \
-#  -t \
-#  --rm \
-#  --net hadoop-net \
-#  --name $name \
-#  -h $name \
-#  hadoop-benchmark/self-balancing-mt \
-#  run \
-#  hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.7.1.jar $@"
-cmd="docker $controller_conn exec controller hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.7.1.jar $@"
+cmd="docker $controller_conn run \
+  -t \
+  --rm \
+  --net hadoop-net \
+  --name $name \
+  -h $name \
+  hadoop-benchmark/self-balancing-mt \
+  run \
+  hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.7.1.jar $@"
+#cmd="docker $controller_conn exec controller hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.7.1.jar $@"
 $cmd
